0e9564993fa80f631c5d9b1dfd02e9f7ed92fa03
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-05-30 00:24:35 +0100

    feat: add accessLog to morgan

--- index.ts
+++ index.ts
@@ -5,28 +5,27 @@ import dotenv from "dotenv";
 import express from "express";
 import "express-async-errors";
 import morgan from "morgan";
-import { useErrorHandler, useNotFound, useRateLimiter } from "./middlewares/";
+import { useErrorHandler, useNotFound } from "./middlewares/";
 import corsOptions from "./utils/corsOptions";
+import logger from "./utils/logger";
+import accessLogStream from "./utils/accessLog";
 dotenv.config();
 
 const PORT = process.env.PORT || 2800;
 const app = express();
-
+const morganConfig =
+  ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"';
 
 app.use(cors(corsOptions));
 
 app.use(cookieParser());
 app.use(bodyParser.json({}));
-app.use(morgan("dev"));
+app.use(morgan(morganConfig, { stream: accessLogStream }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(useRateLimiter);
-
-
 
 app.use(useNotFound);
 app.use(useErrorHandler);
 
-
 export const server = app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  logger.info(`Server running on port ${PORT}`);
 });
--- utils/logger.ts
+++ utils/logger.ts
@@ -1,8 +1,12 @@
 import winston from "winston";
 
+const { combine, timestamp, json } = winston.format;
+
+
+
 const logger = winston.createLogger({
   level: "info",
-  format: winston.format.json(),
+  format: combine(timestamp(), json()),
   defaultMeta: { service: "user-service" },
   transports: [
     new winston.transports.File({
