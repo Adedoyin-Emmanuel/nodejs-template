c30bac723f4aeacfd08ec0249e87e352d471357c
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-05-30 09:30:25 +0100

    fix: fixed logger issues

--- utils/logger.ts
+++ utils/logger.ts
@@ -1,8 +1,24 @@
 import winston from "winston";
+import fs from "fs";
+import path from "path";
 
 const { combine, timestamp, json } = winston.format;
 
+const logDir = "logs";
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
 
+const errorLogPath = path.join(logDir, "error.log");
+const combinedLogPath = path.join(logDir, "combined.log");
+
+if (!fs.existsSync(errorLogPath)) {
+  fs.writeFileSync(errorLogPath, "");
+}
+
+if (!fs.existsSync(combinedLogPath)) {
+  fs.writeFileSync(combinedLogPath, "");
+}
 
 const logger = winston.createLogger({
   level: "info",
@@ -10,13 +26,12 @@ const logger = winston.createLogger({
   defaultMeta: { service: "user-service" },
   transports: [
     new winston.transports.File({
-      dirname: "logs",
+      dirname: logDir,
       filename: "error.log",
       level: "error",
     }),
-
     new winston.transports.File({
-      dirname: "logs",
+      dirname: logDir,
       filename: "combined.log",
     }),
   ],
