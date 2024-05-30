6b151089cd1671df3b3ecd38033d47faee5cfa5a
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-05-30 02:26:21 +0100

    feat: add image upload middleware

--- constants/app/index.ts
+++ constants/app/index.ts
@@ -1,3 +1,4 @@
 export const GLOBAL_RATE_LIMIT_WINDOW_MS = 60 * 1000;
 export const GLOBAL_REQUEST_PER_MINUTE = 100;
-
+export const ALLOWED_FILE_UPLOAD_EXTENSIONS = ["png", "jpeg", "jpg"];
+export const MAX_FILE_SIZE = 1024 * 1024 * 5;
